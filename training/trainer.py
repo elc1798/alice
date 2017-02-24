ff7ff119122b3f652c5376458de3a159266a47c1
elc1798 <subliminalmau5@gmail.com>
2017-02-24 19:25:00 +0000

    Add data amplification technique to improve results

--- training/trainer.py
+++ training/trainer.py
@@ -4,19 +4,21 @@ import glob
 
 from alicized_models import CommandMatchingModel
 
+to_lower = lambda s : s.lower()
+
 def load_data(data_folder):
     """
     Returns 2 lists: the hot list and cold list
     """
+    print "[ DATA LOAD ] Loading data from %s..." % (data_folder,)
     hot, cold = None, None
-    to_lower = lambda s : s.lower()
     with open(os.path.join(data_folder, "true.txt")) as f:
         hot = map(to_lower, f.read().strip().split('\n'))
     with open(os.path.join(data_folder, "false.txt")) as f:
         cold = map(to_lower, f.read().strip().split('\n'))
     return hot, cold
 
-def get_classifier(model_folder):
+def get_classifier(model_folder, dataset):
 
     # The model name is the folder name without the _data at the end, in all
     # caps, and with the extension '.model'
@@ -28,8 +30,7 @@ def get_classifier(model_folder):
         with open(MODEL_PATH, 'r') as MODEL_FILE:
             return pickle.load(MODEL_FILE)
     else:
-        hot, cold = load_data(model_folder)
-        model = CommandMatchingModel( [hot, cold] , shuffle=True, train=True, name=MODEL_FILENAME)
+        model = CommandMatchingModel( dataset , shuffle=True, train=True, name=MODEL_FILENAME)
 
         with open(MODEL_PATH, 'w') as MODEL_FILE:
             pickle.dump(model, MODEL_FILE)
@@ -40,10 +41,31 @@ def get_training_list():
 
 if __name__ == "__main__":
 
+    training_list = get_training_list()
+    # Data amplification
+
+    amplified_data = {}
+    for trainee in training_list:
+        hot, cold = load_data(trainee)
+        amplified_data[trainee] = [ hot, cold ]
+
+    for trainee in training_list:
+        for other in training_list:
+            if trainee == other:
+                continue
+            amplified_data[trainee][1] += amplified_data[other][0] # Add the other's hot to our cold
+
+    with open("nonsense.txt") as f:
+        tmp = map(to_lower, f.read().strip().split('\n'))
+        for trainee in amplified_data:
+            amplified_data[trainee][1] += tmp
+
+    print amplified_data
+
     build_fail = [ False, "" ]
 
-    for trainee in get_training_list():
-        model = get_classifier(trainee)
+    for trainee in amplified_data:
+        model = get_classifier(trainee, amplified_data[trainee])
 
         failcount = 0
         num_tests = 0
@@ -62,7 +84,7 @@ if __name__ == "__main__":
             build_fail[1] = "\n".join( (build_fail[1], "Errors in Model %s" % (trainee,)) )
         print "Model %s failed %d out of %d tests" % (trainee, failcount, num_tests)
 
-        if build_fail[0]:
-            print build_fail[1]
-            assert(False)
+    if build_fail[0]:
+        print build_fail[1]
+        assert(False)
 
