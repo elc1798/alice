2c1ece14f3947c74919c88e1a36865e75f680f1f
elc1798 <subliminalmau5@gmail.com>
2017-02-24 18:50:00 +0000

    Add pickle obj-serialization for model

--- training/kill_active_window_model/trainer.py
+++ training/kill_active_window_model/trainer.py
@@ -1,5 +1,7 @@
-# import spacy
 import random
+import pickle
+import os.path
+
 import numpy as np
 
 from sklearn.feature_extraction.text import CountVectorizer
@@ -7,56 +9,52 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import SGDClassifier
 
-# nlp = spacy.load('en')
+MODEL_FILENAME = "KILL_ACTIVE_WINDOW.model"
 
-f = open("data/true.txt")
-g = open("data/false.txt")
+def load_data():
+    hot, cold = None, None
+    with open("data/true.txt") as f:
+        hot = f.read().strip().split('\n')
+    with open("data/false.txt") as f:
+        cold = f.read().strip().split('\n')
+    return hot, cold
 
-# hot = [ nlp(unicode(s)) for s in f.read().strip().split('\n') ]
-# cold = [ nlp(unicode(s)) for s in g.read().strip().split('\n') ]
+def get_classifier():
+    if os.path.isfile(MODEL_FILENAME):
+        print "Using existing model..."
+        with open(MODEL_FILENAME, 'r') as MODEL_FILE:
+            return pickle.load(MODEL_FILE)
+    else:
+        # Get data
+        hot, cold = load_data()
 
-hot = f.read().strip().split('\n')
-cold = g.read().strip().split('\n')
+        # Build training_set as a scikit-learn Bunch
+        data = [ (i, "True") for i in hot ] + [ (i, "False") for i in cold ]
+        random.shuffle(data)
 
-f.close()
-g.close()
+        training_set = {
+            "description" : "dataset for kill_active_window_model",
+            "data" : [ t[0] for t in data ],
+            "target_names" : ["True", "False"],
+            "target" : [ t[1] for t in data ]
+        }
 
-# Build training_set as a scikit-learn Bunch
+        text_classifier = Pipeline(
+            [   ("vect", CountVectorizer()),
+                ("tfidf", TfidfTransformer()),
+                ("clsfr", SGDClassifier(loss="hinge", penalty="l2",
+                    alpha=1e-3, n_iter=5, random_state=42)),
+            ])
+        _ = text_classifier.fit(training_set["data"], training_set["target"])
 
-data = [ (i, "True") for i in hot ] + [ (i, "False") for i in cold ]
-random.shuffle(data)
+        with open(MODEL_FILENAME, 'w') as MODEL_FILE:
+            pickle.dump(text_classifier, MODEL_FILE)
+        return text_classifier
 
-training_set = {
-    "description" : "dataset for kill_active_window_model",
-    "data" : [ t[0] for t in data ],
-    "target_names" : ["True", "False"],
-    "target" : [ t[1] for t in data ]
-}
-
-# print training_set["data"][:5]
-# print training_set["target"][:5]
-
-count_vect = CountVectorizer()
-# X_train_counts = count_vect.fit_transform(training_set["data"])
-# print X_train_counts.shape
-# tfidf_transformer = TfidfTransformer()
-
-text_classifier = Pipeline(
-    [   ("vect", CountVectorizer()),
-        ("tfidf", TfidfTransformer()),
-        ("clsfr", SGDClassifier(loss="hinge", penalty="l2",
-            alpha=1e-3, n_iter=5, random_state=42)),
-    ])
-
-_ = text_classifier.fit(training_set["data"], training_set["target"])
-
-# sanity_check = text_classifier.predict(training_set)
-# print np.mean(sanity_check == training_set["target"])
-
-while True:
-    test = [ str(raw_input("Test Query: ")).lower() ]
-    predicted = text_classifier.predict(test)
-    print "\t\tGuess: " + str(predicted[0])
-
-
+if __name__ == "__main__":
+    text_classifier = get_classifier()
+    while True:
+        test = [ str(raw_input("Test Query: ")).lower() ]
+        predicted = text_classifier.predict(test)
+        print "\t\tGuess: " + str(predicted[0])
 
